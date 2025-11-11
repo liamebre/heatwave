cea15b3b429d91f852017615a397310c18d27b1c
liamebre <164887882+liamebre@users.noreply.github.com>
2025-11-11 13:02:23 -0500

    update
    setting up base UI elements

---
+++ game.gd
@@ -0,0 +1,10 @@
+extends Node
+
+@export var start_screen : Node
+
+func _ready() -> void:
+	pass
+
+
+func _on_start_start() -> void:
+	start_screen.hide()
---
+++ start.gd
@@ -0,0 +1,11 @@
+extends Control
+signal start
+
+func _on_start_button_down() -> void:
+	start.emit()
+
+func _on_menu_button_down() -> void:
+	pass # Replace with function body.
+
+func _on_quit_button_down() -> void:
+	pass # Replace with function body.
