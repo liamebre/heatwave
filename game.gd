93c897a260cdd3fda5954009a0ac310165c9dae6
liamebre <164887882+liamebre@users.noreply.github.com>
2025-11-13 13:26:54 -0500

    update
    fiexed the ui to the camera added boarder made the player move

--- game.gd
+++ game.gd
@@ -1,12 +1,13 @@
 extends Node
 
 @export var start_screen : Node
-@export var world : Node
-
+@export var world_scene : PackedScene
+var world
 func _ready() -> void:
-	pass
+	world = world_scene.instantiate()
+	
 
 func _on_start_start() -> void:
 	start_screen.hide()
-	world.show()
-	
+	world = world_scene.instantiate()
+	add_child(world)
--- player.gd
+++ player.gd
@@ -1,1 +1,14 @@
-extends Node2D
+extends CharacterBody2D
+@export var speed = 200
+var screen_size
+
+func _ready():
+	screen_size = get_viewport_rect().size
+	
+func _process(_delta: float) -> void:
+	get_input()
+	move_and_collide(velocity * _delta)
+	
+func get_input():
+	var input_dir = Input.get_vector("move_left","move_right","move_up","move_down")
+	velocity = input_dir * speed
